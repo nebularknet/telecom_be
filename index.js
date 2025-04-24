4183d14bc50ed57a8ea8afe046f90d044d927685
Mzammilishtiaq <tomuzammilishtiaq@gmail.com>
2025-04-24 19:24:13 +0500

    setup foulder structure

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-import express from 'express'
-
-
-const app = express();
+import app from 'src/app';
+const port = process.env.PORT || 3000;
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import userRoutes from './routes/userRoutes.js';
+import errorHandler from './middlewares/errorHandler.js';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+
+app.use(errorHandler);
+
+export default app;
