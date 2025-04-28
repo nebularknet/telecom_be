2c91fbeec7e4acd65e25b03427e3ebe53c2ef06c
Mzammilishtiaq <tomuzammilishtiaq@gmail.com>
2025-04-28 20:46:04 +0500

    cors error fix

--- index.js
+++ index.js
@@ -2,13 +2,8 @@ const app = require('./src/app.js');
 const port = 3000;
 const dotenv = require('dotenv');
 const mongoDBConnect = require('./src/config/mongodb.js');
-const cors = require('cors')
 dotenv.config();
 
-app.use(cors({
-    origin: "http://localhost:5173",  // your frontend address
-    credentials: true,                // allow cookies if needed
-  }));
 
 mongoDBConnect();
 app.listen(port, () => {
--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const UserRouting= require('./routes/user_route')
 const dotenv = require('dotenv')
+const cors = require('cors')
+
 dotenv.config();
 const app = express();
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/user',UserRouting);
-
 
 
 module.exports = app
